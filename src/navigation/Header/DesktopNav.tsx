40bbf2698ae850a9fc90dc5ac1943279ba4e017f
mbret <bret.maxime@gmail.com>
2025-10-23 14:13:10 +0200

    feat: added plus nav

--- src/navigation/Header/DesktopNav.tsx
+++ src/navigation/Header/DesktopNav.tsx
@@ -1,45 +1,89 @@
 'use client'
 
-import React from 'react'
+import React, { ComponentProps } from 'react'
 
 import type { Header as HeaderType } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
-import { ChevronDown, ExternalLinkIcon, SearchIcon } from 'lucide-react'
+import {
+  ChevronDown,
+  CircleCheckIcon,
+  CircleHelpIcon,
+  CircleIcon,
+  ExternalLinkIcon,
+  SearchIcon,
+} from 'lucide-react'
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
+import Link from 'next/link'
+import { cn } from '@/components/ui'
+
+function ListItem({ url, isExternal, className, ...rest }: ComponentProps<typeof CMSLink>) {
+  console.log(url, isExternal)
+
+  return (
+    <CMSLink
+      appearance="inline"
+      url={url}
+      isExternal={isExternal}
+      className={cn(className, 'flex-row items-center gap-2')}
+      {...rest}
+    >
+      {/* TODO: Add a search icon to the search link */}
+      {url === '/search' ? (
+        <SearchIcon
+          className="relative top-px ml-1 h-4 w-4 transition duration-200 group-data-[state=open]:rotate-180"
+          aria-hidden="true"
+        />
+      ) : isExternal ? (
+        <ExternalLinkIcon
+          className="relative top-px ml-1 h-4 w-4 transition duration-200 group-data-[state=open]:rotate-180"
+          aria-hidden="true"
+        />
+      ) : null}
+    </CMSLink>
+  )
+}
 
 export const DesktopNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
+  const alwaysVisibleItems = navItems.slice(0, 3).reverse()
+  const restItems = navItems.slice(3).reverse()
 
   return (
     <>
-      <NavigationMenu className="max-md:hidden">
-        <NavigationMenuList className="flex-wrap">
-          {navItems.map(({ link }, i) => {
+      <NavigationMenu viewport={false} className="max-md:hidden">
+        <NavigationMenuList className="flex-wrap ">
+          {restItems.length > 0 && (
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Plus</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[200px] gap-4">
+                  <li>
+                    {restItems.map(({ link }, i) => {
+                      return (
+                        <NavigationMenuLink key={i} asChild>
+                          <ListItem {...link} />
+                        </NavigationMenuLink>
+                      )
+                    })}
+                  </li>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          )}
+          {alwaysVisibleItems.map(({ link }, i) => {
             return (
               <NavigationMenuItem key={i}>
                 <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                  <CMSLink {...link} appearance="inline">
-                    {/* TODO: Add a search icon to the search link */}
-                    {link.url === '/search' ? (
-                      <SearchIcon
-                        className="relative top-px ml-1 h-4 w-4 transition duration-200 group-data-[state=open]:rotate-180"
-                        aria-hidden="true"
-                      />
-                    ) : null}
-                    {link.isExternal ? (
-                      <ExternalLinkIcon
-                        className="relative top-px ml-1 h-4 w-4 transition duration-200 group-data-[state=open]:rotate-180"
-                        aria-hidden="true"
-                      />
-                    ) : null}
-                  </CMSLink>
+                  <ListItem {...link} />
                 </NavigationMenuLink>
               </NavigationMenuItem>
             )
